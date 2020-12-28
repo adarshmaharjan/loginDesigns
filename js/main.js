6cfeb9e63fc6ec4399ee84999c73c5d56f80deaf
adarshmaharjan <40669144+adarshmaharjan@users.noreply.github.com>
2020-12-28 19:01:59 +0545

    first commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+function openNav(){
+    document.getElementById('myNav').style.width = '100%';
+
+}
+function closeNav(){ 
+    document.getElementById("myNav").style.width = '0%';
+}
+//jQuery
+// $(window).on('load', function(){
+//     $('.loading-content-1').delay(500).fadeOut();
+//     $('.loading-content-2').delay(1500).fadeOut();
+//     $('.loading-content-3').delay(2500).fadeOut();
+//     $('#loading').delay(3000).fadeOut();
+
+// });
+//Navigation
