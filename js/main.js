298b0c089bf1d2b2cf9874af8624e6715a0b6365
adarshmaharjan <40669144+adarshmaharjan@users.noreply.github.com>
2020-12-30 08:00:55 +0545

    preloader fixes

--- js/main.js
+++ js/main.js
@@ -5,12 +5,26 @@ function openNav(){
 function closeNav(){ 
     document.getElementById("myNav").style.width= '0%';
 }
-//jQuery
-// $(window).on('load', function(){
-//     $('.loading-content-1').delay(500).fadeOut();
-//     $('.loading-content-2').delay(1500).fadeOut();
-//     $('.loading-content-3').delay(2500).fadeOut();
-//     $('#loading').delay(3000).fadeOut();
+jQuery
+$(window).on('load', function(){
+    $('.loading-content-1').delay(500).fadeOut('slow',function(){
+        $(this).remove();
+    });
+   
+    // $('.loading-content-3').delay(2500).fadeOut();
+    $('.loading').delay(1000).fadeOut();
+
+});
+// jQuery(document).ready(function($) {  
+//     $(window).load(function(){
+//             $('.loading-content-1').fadeOut('slow',function(){
+//                 $(this).remove();
+//             });
+//     });
+//     $(window).load(function(){
+//         $('#loading').fadeOut('slow',function(){
+//             $(this).remove();
+//         });
+// });
 
 // });
-//Navigation
